f6f332f8d89b00a4613101c8c20a30bff354844b
Eddie Chang <kalcifer7319@gmail.com>
2022-12-12 20:32:24 +0800

    Define a interface for objects which are able to perform memory banking.

---
+++ src/io/bankable.cpp
@@ -0,0 +1,13 @@
+#include "bankable.hpp"
+
+namespace gameboy::io {
+    std::uint8_t Mbc::read(int address) const
+    {
+        return 0;
+    }
+
+    void Mbc::write(int address, std::uint8_t value)
+    {
+        
+    }
+}
