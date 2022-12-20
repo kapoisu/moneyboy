cf04a1dbcbdc55b05d202d97ef270def718f1353
Eddie Chang <kalcifer7319@gmail.com>
2022-12-20 19:54:57 +0800

    Integrate the ppu into the Emulator class

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -1,18 +1,46 @@
 #include "emulator.hpp"
+#include "io/bus.hpp"
+#include "io/cartridge.hpp"
 #include "SDL.h"
+#include <chrono>
+#include <iostream>
 
 namespace gameboy {
-    Emulator::Emulator(std::shared_ptr<io::Bus>&& shared_bus) : sm83{shared_bus}
+    Emulator::Emulator()
     {
         if (SDL_Init(SDL_INIT_VIDEO) < 0) {
             throw std::runtime_error{SDL_GetError()};
         }
     }
 
+    void Emulator::load_game()
+    {
+        using io::BootLoader;
+        using io::Bus;
+        using io::Cartridge;
+        using io::CartridgeBanking;
+
+        auto p_cartridge{std::make_unique<Cartridge>("res/Tetris (World) (Rev A).gb")};
+#ifndef SKIP_BOOT_PROCESS
+        auto p_boot_loader{std::make_unique<BootLoader>("res/DMG_boot")};
+        p_boot_loader->capture_cartridge(std::move(p_cartridge));
+        auto p_address_bus{std::make_shared<Bus>(CartridgeBanking{std::move(p_boot_loader)})};
+#else
+        auto p_address_bus{std::make_shared<Bus>(CartridgeBanking{std::move(cartridge)})};
+#endif
+        p_sm83 = std::make_unique<cpu::Core>(p_address_bus);
+        p_lcd = std::make_unique<ppu::Lcd>(p_address_bus);
+    }
+
     void Emulator::run()
     {
+        load_game();
+
         using WindowPtr = std::unique_ptr<SDL_Window, void(*)(SDL_Window*)>;
         using RendererPtr = std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)>;
+        using TexturePtr = std::unique_ptr<SDL_Texture, void(*)(SDL_Texture*)>;
+        using Clock = std::chrono::steady_clock;
+        using Timestamp = std::chrono::time_point<Clock>;
 
         constexpr auto position_x{SDL_WINDOWPOS_UNDEFINED};
         constexpr auto position_y{SDL_WINDOWPOS_UNDEFINED};
@@ -27,11 +55,6 @@ namespace gameboy {
             throw std::runtime_error{SDL_GetError()};
         }
 
-        // auto p_surface = SDL_GetWindowSurface(p_window.get());
-        // SDL_Rect* rect{nullptr};
-        // SDL_FillRect(p_surface, rect, SDL_MapRGBA(p_surface->format, 0x00, 0x00, 0x00, 0x00));
-        // SDL_UpdateWindowSurface(p_window.get());
-
         RendererPtr p_renderer{
             SDL_CreateRenderer(p_window.get(), -1, SDL_RENDERER_ACCELERATED),
             [](SDL_Renderer* ptr) { SDL_DestroyRenderer(ptr); }
@@ -41,7 +64,28 @@ namespace gameboy {
             throw std::runtime_error{SDL_GetError()};
         }
 
+        SDL_RenderSetScale(p_renderer.get(), 3, 3);
+        TexturePtr p_texture{
+            SDL_CreateTexture(p_renderer.get(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 160, 144),
+            [](SDL_Texture* ptr) { SDL_DestroyTexture(ptr); }
+        };
+
+        if (!p_texture) {
+            throw std::runtime_error{SDL_GetError()};
+        }
+
+        constexpr int cycles_per_frame{70224};
+        auto enough_time = [cycles_per_frame](const Timestamp& prev, const Timestamp& current) -> bool {
+            using Seconds = std::chrono::duration<double, std::chrono::seconds::period>;
+            static constexpr double frequency{4.194304e6};
+
+            Seconds seconds_per_frame{(1 / frequency * cycles_per_frame)};
+            return current - prev >= seconds_per_frame;
+        };
+
         SDL_Event e{};
+        Timestamp prev{Clock::now()};
+        int cycle{};
         bool quit{false};
         while (!quit) {
             while (SDL_PollEvent(&e) != 0) {
@@ -50,8 +94,24 @@ namespace gameboy {
                 }
             }
 
-            //Update screen
-            SDL_RenderPresent(p_renderer.get());
+            if (cycle < cycles_per_frame) {
+                p_sm83->tick();
+                for (auto i{0}; i < 4; ++i) {
+                    p_lcd->update(*p_renderer, *p_texture);
+                }
+
+                cycle += 4;
+            }
+
+            if (Timestamp current{Clock::now()}; cycle == cycles_per_frame) {
+                if (enough_time(prev, current)) {
+                    prev = current;
+                    cycle = 0;
+                }
+                else {
+                    std::cout << "too fast";
+                }
+            }
         }
     }
 
--- src/emulator.hpp
+++ src/emulator.hpp
@@ -3,18 +3,20 @@
 
 #include <memory>
 #include "cpu/core.hpp"
+#include "ppu/lcd.hpp"
 #include "io/bus.hpp"
 
 namespace gameboy {
     class Emulator {
     public:
-        explicit Emulator(std::shared_ptr<io::Bus>&& shared_bus);
+        explicit Emulator();
         void load_game();
         void save_game();
         void run();
         ~Emulator();
     private:
-        cpu::Core sm83;
+        std::unique_ptr<cpu::Core> p_sm83{};
+        std::unique_ptr<ppu::Lcd> p_lcd{};
     };
 }
 
