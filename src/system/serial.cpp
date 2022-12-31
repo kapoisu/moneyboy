bba716e7562f8f26a66efd71f164d5b020fbbbdf
Eddie Chang <kalcifer7319@gmail.com>
2023-01-01 07:56:55 +0800

    Change the structure of Serial registers

--- src/system/serial.cpp
+++ src/system/serial.cpp
@@ -1,6 +1,6 @@
 #include "serial.hpp"
 #include <array>
-#include <bitset>
+#include <limits>
 #include <iostream>
 #include <stdexcept>
 
@@ -8,6 +8,12 @@ namespace gameboy::system {
     enum Register {
         sb = 0xFF01,
         sc = 0xFF02
+    };
+
+    enum Control {
+        internal_clock = 0,
+        clock_speed = 1,
+        transfering = 7
     };
 
     Serial::Serial(std::shared_ptr<Interrupt> shared_interrupt) : p_interrupt{std::move(shared_interrupt)}
@@ -31,7 +37,12 @@ namespace gameboy::system {
 
     bool Serial::is_transfering() const
     {
-        return (transfer_control >> 7) & 1;
+        return transfer_control.test(transfering);
+    }
+
+    bool Serial::is_sender() const
+    {
+        return transfer_control.test(internal_clock);
     }
 
     std::uint8_t Serial::read(int address) const
@@ -40,7 +51,7 @@ namespace gameboy::system {
             case sb:
                 return transfer_data;
             case sc:
-                return transfer_control;
+                return static_cast<std::uint8_t>(transfer_control.to_ulong());
             default:
                 throw std::out_of_range{"Invalid address."};
         }
@@ -53,7 +64,7 @@ namespace gameboy::system {
                 transfer_data = value;
                 return;
             case sc:
-                transfer_control = value;
+                transfer_control = value | 0b0111'1110;
                 return;
             default:
                 throw std::out_of_range{"Invalid address."};
--- src/system/serial.hpp
+++ src/system/serial.hpp
@@ -1,6 +1,7 @@
 #ifndef SYSTEM_SERIAL_H
 #define SYSTEM_SERIAL_H
 
+#include <bitset>
 #include <cstdint>
 #include <memory>
 #include "io/bus.hpp"
@@ -11,11 +12,13 @@ namespace gameboy::system {
     public:
         Serial(std::shared_ptr<Interrupt> shared_interrupt);
         void tick();
-        bool is_transfering() const;
 
         virtual std::uint8_t read(int address) const override;
         virtual void write(int address, std::uint8_t value) override;
     private:
+        bool is_transfering() const;
+        bool is_sender() const;
+
         int counter{};
         char transfer_data{};
 
@@ -23,7 +26,7 @@ namespace gameboy::system {
             bit 7: Transfer Start Flag (0=No Transfer, 1=Start)
             bit 0: Shift Clock (0=External Clock, 1=Internal Clock)
         */
-        std::uint8_t transfer_control{0b0111'1110};
+        std::bitset<8> transfer_control{0b0111'1110};
 
         std::shared_ptr<Interrupt> p_interrupt;
     };
